e5e8a5e35b29c14720e13b11463179e2b0ae20e5
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-06-29 18:06:46 -0300

    feat: session validation

--- corepanel/src/pages/home.tsx
+++ corepanel/src/pages/home.tsx
@@ -1,7 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Home() {
+  const navigate = useNavigate();
+  const user = checkUser();
+
+
+  function checkUser() {
+    const value = sessionStorage.getItem("acessType");
+    console.log(value)
+    if (value == null) {
+      console.log("voltou?");
+      setTimeout(() => { // so that React can load everything in time 
+        navigate('/login'); 
+      }, (200));
+    }
+    return value;
+  }
+
 
   return (
     <div>
+      {user}
       HOME
     </div>
   );
