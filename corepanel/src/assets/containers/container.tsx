636d55e70a143b38da712d9f8ca3057831dd500b
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-07-07 14:12:23 -0300

    refacture: home & components

--- corepanel/src/assets/containers/container.tsx
+++ corepanel/src/assets/containers/container.tsx
@@ -7,11 +7,12 @@ export const ContainerGridLayout = {
 interface PropsContainer {
   message: string,
   layout?: typeof ContainerGridLayout[keyof typeof ContainerGridLayout],
+  height?: string
 }
-export default function Container({ message, layout = ContainerGridLayout.default }: PropsContainer) {
+export default function Container({ message, layout = ContainerGridLayout.default, height }: PropsContainer) {
   
   return (
-    <div className={`bg-slate-300 rounded-4xl p-6 border-2 border-slate-400 ${layout}`}>
+    <div className={`bg-slate-300 rounded-4xl p-6 border-2 border-slate-400 ${layout} ${height}`}>
       <p>{message}</p>
     </div>
   );
--- corepanel/src/pages/home.tsx
+++ corepanel/src/pages/home.tsx
@@ -1,19 +1,50 @@
 import { useNavigate } from "react-router-dom";
 import { validateSession } from "../helpers/session_helper.tsx"
-import Container, { ContainerGridLayout } from "../assets/container.tsx";
+import { ContactRound, Lock, Shield, ShieldHalf, User } from "lucide-react";
 
 export default function Home() {
   const navigate = useNavigate();
   const user = validateSession();
-
+  const mainColor = "forestGreen"
 
   return (
-    <div className="h-screen w-screen bg-slate-200 grid grid-cols-2 grid-rows-[1fr_1fr_auto] p-14 gap-4">
-      <Container message={`Seja bem-vindo ${user}`}/>
-      <Container message={"Atualizações recentes (gráficos)"} />
-      <Container message={"Visualizar dados"} />
-      <Container message={"Usuários"} />
-      <Container message={"Realizar ações"} layout={ContainerGridLayout.full} />
+    <div className="h-screen w-full bg-slate-100 p-6 lg:p-14">
+      <div className="h-full grid grid-cols-1 sm:grid-cols-2 gap-6">
+        <div className={`bg-slate-200 rounded-4xl p-6 border-2 border-slate-300 flex`}>
+          <div className="h-full w-1/2 flex flex-col items-center justify-center text-2xl">
+            <div>
+              <User size={100} color={mainColor} />
+            </div>
+            <p>Seja bem-vindo {user}</p>
+          </div>
+          <div className="h-full w-1/2 flex flex-col items-start justify-evenly text-xl">
+            <div className="flex items-center">
+              <ContactRound size={26} color={mainColor} />
+              <p>Perfil: {user}</p>
+            </div>
+            <div className="flex items-center">
+              <ShieldHalf size={26} color={mainColor} />
+              <p>Status da sessão: Ativa</p>
+            </div>
+            <div className="flex items-center">
+              <Lock size={26} color={mainColor} />
+              <p>Permissões: Total</p>
+            </div>
+          </div>
+        </div>
+        <div className={`bg-slate-200 rounded-4xl p-6 border-2 border-slate-300 flex`}>
+          <p>Atualizações recentes</p>
+        </div>
+        <div className={`bg-slate-200 rounded-4xl p-6 border-2 border-slate-300 flex`}>
+          <p>Visualizar Dados</p>
+        </div>
+        <div className={`bg-slate-200 rounded-4xl p-6 border-2 border-slate-300 flex`}>
+          <p>Usuários</p>
+        </div>
+        <div className={`bg-slate-200 rounded-4xl p-6 border-2 border-slate-300 lg:col-span-2`}>
+          <p>Ações rápidas</p>
+        </div>
+      </div>
     </div>
   );
 }
