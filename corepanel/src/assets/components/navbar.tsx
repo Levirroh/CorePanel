08d5972cf30ed5a84b1b2dd1e93a44e4bbf9cfc6
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-07-26 16:12:31 -0300

    feat: new navbar items

--- corepanel/src/assets/components/navbar.tsx
+++ corepanel/src/assets/components/navbar.tsx
@@ -1,4 +1,4 @@
-import { Home, LogOut } from "lucide-react";
+import { Archive, FileExclamationPoint, Home, LogOut, NotebookPen, Users } from "lucide-react";
 import NavbarItem from "./navbarItem";
 
 export default function Navbar({ isActive: boolean, activeMenu: Number }) {
@@ -8,18 +8,38 @@ export default function Navbar({ isActive: boolean, activeMenu: Number }) {
       url: "/home",
       title: "Home",
       icon: Home
-    }
+    },
+    {
+      url: "activities",
+      title: "Activities",
+      icon: NotebookPen
+    },
+    {
+      url: "/reports",
+      title: "Reports",
+      icon: FileExclamationPoint
+    },
+    {
+      url: "/users",
+      title: "Users",
+      icon: Users
+    },
+    {
+      url: "/records",
+      title: "Records",
+      icon: Archive
+    },
   ]
 
   return (
     <div className={`bg-slate-800 text-white text-sm h-full w-60 flex flex-col gap-2 p-2 justify-between`}>
       <div className="w-full h-[92%]">
         {pages.map((item) => (
-          <NavbarItem key={item.url} url={item.url} title={item.title} Icon={item.icon}/>
+          <NavbarItem key={item.url} url={item.url} title={item.title} Icon={item.icon} />
         ))}
       </div>
       <div className="w-full h-[8%]">
-        <NavbarItem key={"/login"} url={"/login"} title={"Sair"} Icon={LogOut}/>
+        <NavbarItem key={"/login"} url={"/login"} title={"Sair"} Icon={LogOut} />
       </div>
     </div>
   );
