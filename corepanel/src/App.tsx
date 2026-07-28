f29edf86c879305df249b443a66c3c0f4a1982a9
Levirroh <johann.gr14@gmail.com>
2026-07-27 21:22:10 -0300

    style: default background color

--- corepanel/src/App.tsx
+++ corepanel/src/App.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
-    <main className="min-h-screen w-screen bg-slate-950">
+    <main className="min-h-screen w-screen bg-slate-100">
       <Outlet />
     </main>
   );
--- corepanel/src/pages/users.tsx
+++ corepanel/src/pages/users.tsx
@@ -1,7 +1,6 @@
 export default function Users() {
   return (
-    <div className="w-screen h-screen bg-linear-to-tr from-slate-950 from-60% to-orange-700 bg-size-[200%_200%] animate-gradient text-slate-300
-      flex items-center justify-center">
+    <div className="w-screen h-screen bg-slate-100">
     </div>
   );
 }
