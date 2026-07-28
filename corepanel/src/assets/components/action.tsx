a30fc6df40e0e58c5eb7b711c84daa578dfbc542
Levirroh <johann.gr14@gmail.com>
2026-07-28 20:41:36 -0300

    style: minor changes

--- corepanel/src/assets/components/action.tsx
+++ corepanel/src/assets/components/action.tsx
@@ -11,7 +11,8 @@ interface ActionProps {
 export default function ActionButton({ url, title, color, textColor, borderColor, Icon }: ActionProps) {
   return (
     <a href={url || "#"} className="flex justify-center items-center">
-      <div className={`bg-none rounded-xl ${color} ${textColor} text-sm text-center items-center p-4 gap-1 h-32 w-32 border-2 ${borderColor} flex flex-col justify-center hover:scale-105 transition-transform duration-200`}>
+      <div className={`bg-none rounded-xl ${color} ${textColor} text-sm text-center items-center p-4 gap-1 h-32 w-32 border-2 ${borderColor}
+       flex flex-col justify-center hover:scale-105 transition-transform duration-200 shadow-lg transition-shadow duration-300`}>
         <div className="font-semibold">
           {title || <Icon className={`h-6 w-6 ${textColor}`} />}
         </div>
