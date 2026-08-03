141aa3527f73ecd88ce554208e5105d68fc6e19b
Levirroh <johann.gr14@gmail.com>
2026-08-03 19:13:51 -0300

    update: fixated user to IUser

--- corepanel/src/assets/components/containers/userContainer.tsx
+++ corepanel/src/assets/components/containers/userContainer.tsx
@@ -1,16 +1,20 @@
-import image from "../../images/random-person-01.jpeg"
+import type { IUser } from "../../../data/class/user";
 import UserPictureContainer from "./userPictureContainer"
 
-export default function UserContainer() {
+interface UserProp {
+  user: IUser
+}
+
+export default function UserContainer({user}: UserProp) {
   return (
     <div className="bg-slate-100 rounded-xl p-6 border-2 w-full border-slate-200 flex relative">
-      <UserPictureContainer imagePath={image} />
+      <UserPictureContainer imagePath={user.profileImage} />
       <div className="p-2">
-        <p className="font-semibold text-[0.7vw]">Márcio José</p>
-        <p className="text-slate-700">Admin</p>
+        <p className="font-semibold text-[0.7vw]">{user.name}</p>
+        <p className="text-slate-700">{user.role}</p>
       </div>
       <div className="absolute bottom-2 right-4 text-slate-700">
-        <p>Último acesso às 11:02</p>
+        <p>Último acesso às {user.lastAccessAt}</p>
       </div>
     </div>
   );
