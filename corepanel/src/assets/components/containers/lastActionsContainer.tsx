6facb2c71fa4437b688055d640f3762e4d0de33d
Johann Gossen Ruth <johann.gr14@gmail.com>
2026-08-09 19:08:04 -0300

    feat: getting user related to action

--- corepanel/src/assets/components/containers/lastActionsContainer.tsx
+++ corepanel/src/assets/components/containers/lastActionsContainer.tsx
@@ -1,17 +1,17 @@
 import type { IActions } from "../../../data/class/action";
+import type { IUser } from "../../../data/class/user";
 
 interface LastActionsProps {
-  action: IActions
+  action: IActions,
+  user: IUser | undefined
 }
 
 
-export default function LastActionsContainer({action}: LastActionsProps) {
+export default function LastActionsContainer({action, user}: LastActionsProps) {
 
-
-  
   return (
     <div className={`flex flex-col`}>
-      <p>{action.description}</p>
+      <p><a href={`users/${user?.id}`}>{user?.name}</a> {action.description}</p>
     </div>
   );
 }
--- corepanel/src/assets/components/containers/lastActionsHomeContainer.tsx
+++ corepanel/src/assets/components/containers/lastActionsHomeContainer.tsx
@@ -1,15 +1,17 @@
 import LastActionsContainer from "./lastActionsContainer";
 import actionService  from "../../../service/mock/api/actionsService";
+import userService  from "../../../service/mock/api/userService";
 
 export default function LastActionsHomeContainer() {
 
   const actions = actionService();
+  const users = userService();
 
   return (
     <div className={`flex flex-col`}>
-      <LastActionsContainer action={actions[0]}/>
-      <LastActionsContainer action={actions[1]}/>
-      <LastActionsContainer action={actions[2]}/>
+      <LastActionsContainer action={actions[0]} user={users.findLast(x => x.id == actions[0].userId)}/>
+      <LastActionsContainer action={actions[1]} user={users.findLast(x => x.id == actions[1].userId)}/>
+      <LastActionsContainer action={actions[2]} user={users.findLast(x => x.id == actions[2].userId)}/>
     </div>
   );
 }
