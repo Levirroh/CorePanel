e9e4e66f8f2bef4d45fd64b98d9abd487c09eda4
Levirroh <johann.gr14@gmail.com>
2026-08-10 08:47:11 -0300

    style: better response to user on hover

--- corepanel/src/assets/components/containers/lastActionsContainer.tsx
+++ corepanel/src/assets/components/containers/lastActionsContainer.tsx
@@ -12,7 +12,7 @@ interface LastActionsProps {
 export default function LastActionsContainer({ action, user }: LastActionsProps) {
 
   return (
-    <div className={`flex rounded-md p-4 bg-slate-100 shadow-xs/20 justify-between items-center`}>
+    <div className={`flex rounded-md p-4 bg-slate-100 shadow-xs/20 hover:shadow-2xl/40 justify-between items-center hover:scale-103 transition duration-200 hover:bg-slate-800 hover:text-white`}>
       <div className="">
         <p><a className="font-semibold truncate" href={`users/${user?.id}`}>{user?.name}:</a> {action.description}</p>
         <p className="text-xs text-slate-500 font-semibold">{formatDate(action.createdAt)}</p>
