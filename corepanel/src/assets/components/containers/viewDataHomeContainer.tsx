d77b90063ba8c9db3b67dd10e707ae7f89638624
Levirroh <johann.gr14@gmail.com>
2026-08-11 14:50:10 -0300

    feat: intial chart

--- corepanel/src/assets/components/containers/viewDataHomeContainer.tsx
+++ corepanel/src/assets/components/containers/viewDataHomeContainer.tsx
@@ -1,13 +1,17 @@
 import { PieChart } from '@mui/x-charts/PieChart';
+import { getReports } from "../../../service/mock/api/reportsService";
 
 export default function ViewDataHomeContainer() {
+  const reports = getReports();
 
   const data = [
-    { label: 'Group A', value: 1, color: '#0088FE' },
-    { label: 'Group B', value: 2, color: '#00C49F' },
-    { label: 'Group C', value: 4, color: '#FFBB28' },
-    { label: 'Group D', value: 8, color: '#FF8042' },
+    { label: 'Generated', value: reports.filter(x => x.status == 1).length, color: '#0088FE' },
+    { label: 'Processing', value: reports.filter(x => x.status == 2).length, color: '#00C49F' },
+    { label: 'Failed', value: reports.filter(x => x.status == 3).length, color: '#FF8042' },
+    { label: 'Archived', value: reports.filter(x => x.status == 4).length, color: '#FFBB28' },
   ];
+
+
 
   const settings = {
     margin: { right: 5 },
@@ -18,7 +22,19 @@ export default function ViewDataHomeContainer() {
 
   return (
     <div className={`flex flex-col gap-2`}>
-      <PieChart series={[{ innerRadius: 50, outerRadius: 100, data, arcLabel: 'value' }]}
+      <PieChart series={[
+        {
+          data: [...data],
+          innerRadius: 50,
+          outerRadius: 100,
+          paddingAngle: 3,
+          cornerRadius: 8,
+          startAngle: 0,
+          endAngle: 360,
+          cx: 95,
+          cy: 95,
+        }
+      ]}
         {...settings}
       />
     </div>
